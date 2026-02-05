716560616c9910e71eedfa88d1b58dd361e62e28
Rajesh patel <rajeshcode07@gmail.com>
2026-02-05 21:00:18 +0530

    Removed experience section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Aboutme from "@/components/aboutme";
 import Contact from "@/components/contact";
-import Experience from "@/components/experience";
+// import Experience from "@/components/experience";
 import GitHubGraph from "@/components/githubgraph";
 import Header from "@/components/header";
 import Hero from "@/components/hero";
@@ -11,7 +11,7 @@ export default function Home() {
     <div className=" border  border-(--border) mb-10  md:mx-10 md:mb-10 lg:mx-20 xl:mx-30 3xl:mx-50 4xl:mx-80 lg:mb-20 xl:mb-30 2xl:mb-80  box-border ">
       <Header />
       <Hero />
-      <Experience />
+      {/* <Experience /> */}
       <GitHubGraph />
       <Projects />
       <Aboutme />
